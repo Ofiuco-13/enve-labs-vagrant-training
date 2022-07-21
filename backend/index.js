f156b361e376617ab1e8166e14eb37fe7bdf7624
emmanuel martinez <emmamartinzwc@gmail.com>
2022-07-21 17:37:38 -0300

    FIX: adds puppeteer stealth mode to scrap ticketmaster seats

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
 const Xvfb = require("xvfb");
 const fs = require("fs");
-
-console.log("Running...");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+const express = require("express");
+const app = express();
+const port = 3000;
 
 (async () => {
   var xvfb = new Xvfb({
@@ -13,30 +16,42 @@ console.log("Running...");
     if (err) console.error(err);
   });
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: null, //otherwise it defaults to 800x600
     args: ["--no-sandbox", "--start-fullscreen", "--display=" + xvfb._display],
   });
 
   try {
+    console.log("APP: Starting...");
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
-    await page.goto(`https://www.wildriftfire.com/tier-list`);
+    await page.goto(
+      `https://www.ticketmaster.com/1st-round-mens-womens-flushing-new-york-08-29-2022/event/1D005C67D2C724AE`
+    );
+    await page
+      .waitForSelector(".modal-dialog__button.button", {
+        visible: true,
+        timeout: 90000,
+      })
+      .then(() => console.log("Clicking modal button..."));
 
-    const grabChampions = await page.evaluate(() => {
-      const tierList = document.querySelectorAll(
-        ".wf-tier-list__tiers__block div a span"
-      );
-      let champions = [];
-      tierList.forEach((tag) => {
-        champions.push(tag.innerText);
+    await page
+      .click(".modal-dialog__button.button")
+      .then(() => console.log("Clicked!"))
+      .then(() => console.log("Grabbing seats..."));
+
+    const grabTickets = await page.evaluate(() => {
+      const ticketList = document.querySelectorAll(".quick-picks__item-desc");
+      let tickets = [];
+      ticketList.forEach((tag) => {
+        tickets.push(tag.innerText);
       });
-      return champions;
+      return tickets;
     });
 
-    await page.screenshot({ path: "tierlist.png", fullPage: true });
-    console.log(grabChampions);
-    fs.writeFile("./recipes.json", JSON.stringify(grabChampions), (err) =>
+    await page.screenshot({ path: "event.png", fullPage: true });
+    console.log(`Tickets grabbed: ${grabTickets}`);
+    fs.writeFile("./seats.json", JSON.stringify(grabTickets), (err) =>
       err ? console.log(err) : null
     );
 
@@ -46,3 +61,11 @@ console.log("Running...");
     console.log(error);
   }
 })();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Express: Listening on port: ${3000}`);
+});
